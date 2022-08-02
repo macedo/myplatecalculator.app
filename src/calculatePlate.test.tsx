00bc0d8819adb61bdd0ea9d017ca1fde819a95ef
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2022-08-02 13:54:33 -0300

    test calculator

--- src/calculatePlate.test.tsx
+++ src/calculatePlate.test.tsx
@@ -2,10 +2,10 @@ import calculatePlates from './calculatePlates';
 
 describe('Calculate Plates', () => {
     test.each([
-        {weight: 140, availablePlates: [[35, 10], [45, 0], [25, 10], [15, 10], [10, 10]], expected: {35: 4, 25: 0, 15: 0, 10: 0}},
-        {weight: 140, availablePlates: [[45, 10], [35, 0], [25, 10], [15, 10], [10, 10]], expected: {45: 2, 35: 0, 25: 2, 15: 0, 10: 0}},
-        {weight: 320, availablePlates: [[45, 10], [35, 10], [25, 10]], expected: { 45: 6, 35: 0, 25: 2}},
-    ])('calculatePlates($weight, $availablePlates)', ({weight, availablePlates, expected}) => {
+        [140, [[35, 10], [45, 0], [25, 10], [15, 10], [10, 10]], {35: 4, 25: 0, 15: 0, 10: 0}],
+        [140, [[45, 10], [35, 0], [25, 10], [15, 10], [10, 10]], {45: 2, 35: 0, 25: 2, 15: 0, 10: 0}],
+        [320, [[45, 10], [35, 10], [25, 10]], { 45: 6, 35: 0, 25: 2}],
+    ])('calculatePlates(%s, %j)', (weight, availablePlates, expected) => {
             const result = calculatePlates(weight, availablePlates);
             expect(result).toMatchObject(expected);
         }
