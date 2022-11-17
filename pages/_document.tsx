224cc28292fa96f5e6999e545e373bbba4b327df
Rafael Macedo <macedo.rafaelfernandes@gmail.com>
2022-11-17 09:36:03 -0300

    Simple "Add to Homescreen" manifest (PWA)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,7 @@ function Document() {
     <Html className="h-full bg-gray-100">
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
       </Head>
       <body className="h-full">
